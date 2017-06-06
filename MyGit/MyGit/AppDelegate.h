1cc515ecfa7baa0c57f5d39689f42fedd912e88e
lishihua <lishihua@100tal.com>
2017-06-06 20:06:58 +0800

    Hello LSH

---
+++ MyGit/MyGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyGit
+//
+//  Created by 李世华 on 2017/6/6.
+//  Copyright © 2017年 zhikang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
