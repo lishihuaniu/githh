b666231a877c27447455636e22000faa488cb82a
lishihua <lishihua@100tal.com>
2017-06-07 21:24:06 +0800

    LSHSecondVC Unknow

---
+++ MyGit/MyGit/LSHSecondVC.h
@@ -0,0 +1,13 @@
+//
+//  LSHSecondVC.h
+//  MyGit
+//
+//  Created by 李世华 on 2017/6/7.
+//  Copyright © 2017年 zhikang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LSHSecondVC : NSObject
+
+@end
